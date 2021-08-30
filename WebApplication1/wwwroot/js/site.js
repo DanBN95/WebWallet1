77b60987ae281e781019888ff199f6f85ecb1615
DanBN <dbn290895@gmail.com>
2021-08-31 00:59:47 +0300

    Search form Incomes Index

--- WebApplication1/wwwroot/js/site.js
+++ WebApplication1/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function Sort(value) {
+    console.log("Hello World");
+}
+
+document.addEventListener('click', () => {
+    const map = new Map();
+    
+})
