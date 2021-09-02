90a8513bd87086089ff4af81285cc3ff9277845f
Hadar Liberman <73418912+HadarLiberman@users.noreply.github.com>
2021-09-02 13:13:21 +0300

    add

--- WebApplication1/wwwroot/js/coinLore.js
+++ WebApplication1/wwwroot/js/coinLore.js
@@ -1,24 +1,34 @@
 ﻿$(document).ready(function () {
-    $.ajax({
-        url: "https://api.coinlore.net/api/tickers/",
-        type: "GET",
-        success: function (result) {
-            var data = result["data"];
-            for (var i = 0; i < data.length; i++) {
-                var template = '<div class="coinCard"> <p> Coin Name: ' + data[i].name +
-                    '</p><p> Coin Price: ' + data[i].price_usd +
-                    '$</p><p> Change in last 24 hours: '+ data[i].percent_change_24h+'</p> </div>';
-                $('#myDiv').append(template);
+ 
+        $.ajax({
+            url: "https://api.coinlore.net/api/tickers/",
+            type: "GET",
+            success: function (result) {
+                var data = result["data"];
+                for (var i = 0; i < data.length; i++) {
+                    var template =
+                        '<div class="card"> <i class="fab fa-bitcoin card-img-top"></i> <div class="card-body"> <h5 class="card-title">' + data[i].name +
+                        '</h5> <p class="card-text">Coin Price: ' + data[i].price_usd +
+                        '</p> <p class="card-text">  Change in last 24 hours: ' + data[i].percent_change_24h +
+                        '</p> </div> </div>';
+                        
+                    $('#myDiv').append(template);
+                }
+                console.log(result);
+                console.log(result.data.length);
+                console.log(result["data"].length);
+
+
+            },
+            error: function (error) {
+                console.log(error);
             }
-            console.log(result);
-            console.log(result.data.length);
-            console.log(result["data"].length);
-
-
-        },
-        error: function (error) {
-            console.log(error);
-        }
+        })
     })
 
+
+$(function () {
+$('.apiBtn').click(function () {
+    alert("hey");
+})
 })
